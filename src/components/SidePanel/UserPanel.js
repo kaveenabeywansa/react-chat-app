84f42779e0926f2c74e847ac5560040f96f2ff7b
Kaveen Abeywansa <kaveenabeywansa@gmail.com>
2020-06-16 00:31:21 +0530

    Finish Lesson 14

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import firebase from '../../firebase';
-import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
+import React from "react";
+import firebase from "../../firebase";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 
 class UserPanel extends React.Component {
     state = {
-        user: this.props.currentUser
-    }
+        user: this.props.currentUser,
+    };
 
     dropdownOptions = () => [
         {
-            key: 'user',
-            text: <span>Signed in as <strong>{this.state.user.displayName}</strong></span>,
-            disabled: true
+            key: "user",
+            text: (
+                <span>
+                    Signed in as <strong>{this.state.user.displayName}</strong>
+                </span>
+            ),
+            disabled: true,
         },
         {
-            key: 'avatar',
-            text: <span>Change Avatar</span>
+            key: "avatar",
+            text: <span>Change Avatar</span>,
         },
         {
-            key: 'signout',
-            text: <span onClick={this.handleSignOut}>Sign Out</span>
-        }
+            key: "signout",
+            text: <span onClick={this.handleSignOut}>Sign Out</span>,
+        },
     ];
 
     handleSignOut = () => {
         firebase
             .auth()
             .signOut()
-            .then(() => console.log('Signed out'))
-    }
+            .then(() => console.log("Signed out"));
+    };
 
     render() {
         const { user } = this.state;
+        const { primaryColor } = this.props;
 
         return (
-            <Grid style={{ background: '#4c3c4c' }}>
+            <Grid style={{ background: primaryColor }}>
                 <Grid.Column>
-                    <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+                    <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
                         {/* App Header */}
                         <Header inverted floated="left" as="h2">
                             <Icon name="code" />
@@ -44,15 +49,17 @@ class UserPanel extends React.Component {
                         </Header>
 
                         {/* User Dropdown */}
-                        <Header style={{ padding: '0.25em' }} as="h4" inverted>
-                            <Dropdown trigger={
-                                <span>
-                                    <Image src={user.photoURL} spaced="right" avatar />
-                                    {user.displayName}
-                                </span>
-                            } options={this.dropdownOptions()} />
+                        <Header style={{ padding: "0.25em" }} as="h4" inverted>
+                            <Dropdown
+                                trigger={
+                                    <span>
+                                        <Image src={user.photoURL} spaced="right" avatar />
+                                        {user.displayName}
+                                    </span>
+                                }
+                                options={this.dropdownOptions()}
+                            />
                         </Header>
-
                     </Grid.Row>
                 </Grid.Column>
             </Grid>
